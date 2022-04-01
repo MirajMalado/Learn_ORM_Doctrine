e42bcba1434604a8ce90621a966f77cf2e664816
MirajMalado <diallomalado8@gmail.com>
2022-04-01 15:43:48 +0000

    requete sql et dump

--- src/entities/Roles.php
+++ src/entities/Roles.php
@@ -1,18 +1,19 @@
 <?php
-use Doctrine\ORM\Annotation as ORM;
+// use Doctrine\ORM\Annotation as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
 /**
- * @Entity @Table(name="roles")
+ * @ORM\Entity @ORM\Table(name="roles")
  **/
  class Roles
  {
-     /** @Id @Column(type="integer") @GeneratedValue **/
+     /** @ORM\Id @ORM\Column(type="integer") @ORM\GeneratedValue **/
     private $id;
-    /**  @Column(type="string")**/
+    /**  @ORM\Column(type="string")**/
     private $nom;
      /**
      * Many Roles have Many Users.
-     * @ManyToMany(targetEntity="User", mappedBy="roles")
+     * @ORM\ManyToMany(targetEntity="User", mappedBy="roles")
      */
     private $users;
 
